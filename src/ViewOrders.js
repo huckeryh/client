8c4e5bd67593d3d3ba278b5524d30c6b3a78e1d6
huckeryh <150742565+huckeryh@users.noreply.github.com>
2023-11-26 19:43:25 -0500

    goodstuff

---
+++ src/ViewOrders.js
@@ -0,0 +1,16 @@
+
+import React/*, {useState, useEffect }*/ from 'react' 
+import {Link} from 'react'
+
+function Tables() {
+
+  return (
+    <div>
+      <h1>Tables</h1>
+      {/* Link back to the home page */}
+      <Link to="/AddOrders">Go to Add Orders Page</Link>
+    </div>
+  )
+}
+
+export default Tables
