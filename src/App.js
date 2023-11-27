045000e62ac4d88ce3a4edd4d0ee6a6ca33484e7
huckeryh <150742565+huckeryh@users.noreply.github.com>
2023-11-26 19:40:43 -0500

    com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState } from 'react' 
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import AddOrders from './AddOrders'
+import Tables from './ViewOrders'
+import './App.css'
 
 function App() {
+  const [selectedButton, setSelectedButton] = useState(null);
+
+  const handleButtonClick = (buttonNumber) => {
+    setSelectedButton(buttonNumber);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <Router>
+    <div>
+
+      {/* Pull these two from the backend*/}
+      <h2 style={{textAlign:'center'}}>***PULL SERVER NAME***</h2>
+      <h2 style={{textAlign:'center'}}>***PULL RESTAURANT NAME***</h2>
+
+      {/*Horizontal Rule*/}
+      <hr />
+
+
+      <h1 style={{textAlign:'center'}}>Assigned Tables</h1>
+
+
+      <table className="tables-grid">
+        {/*Table Body*/}
+        <tbody>
+          {/*Table Row*/}
+          <tr>
+          {/*Table Data*/}
+          <td className={`table-table ${selectedButton === 1 ? 'selected' : ''}`}>
+              <button onClick={() => handleButtonClick(1)}>Table 1</button>
+            </td>
+
+            <td className={`table-table ${selectedButton === 2 ? 'selected' : ''}`}>
+              <button onClick={() => handleButtonClick(2)}>Table 2</button>
+            </td>
+          </tr>
+          
+          {/*Next table row*/}
+          <tr>
+            <td className={`table-table ${selectedButton === 3 ? 'selected' : ''}`}>
+              <button onClick={() => handleButtonClick(3)}>Table 3</button>
+            </td>
+            <td className={`table-table ${selectedButton === 4 ? 'selected' : ''}`}>
+              <button onClick={() => handleButtonClick(4)}>Table 4</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+        {/*Link based on the selected button*/}
+        <h1 style ={{textAlign:'center'}}><Link to={`/AddOrders/${selectedButton}`}>
+          <button disabled={selectedButton === null}>{selectedButton !== null ? `Add Order for Table ${selectedButton}` : 'Add order'}</button>
+        </Link></h1>
+
+
+        <h3 style ={{textAlign:'center'}}><Link to='ViewOrders'><button>View Orders</button></Link></h3>
+
+        {/*Routing table*/}
+        <Routes>
+          <Route path='/AddOrders/:buttonNumber' element={<AddOrders/>}/>
+          <Route path='/ViewOrders' element ={<Tables/>}/>
+
+      </Routes>
+
     </div>
-  );
+    </Router>
+  )
 }
 
-export default App;
+export default App
+
+      /* {(typeof data.members === 'undefined')? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member,i) => (
+          <h1 style={{ textAlign:'center' }} key={i}>{member}</h1>
+        ))
+      )}*/
+
+        // const [data, setData] = useState([{}])
+
+  // useEffect(() => {
+  //   fetch("/members").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
